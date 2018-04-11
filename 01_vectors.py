8814816243062427a94c2dbb09a1c60099ff9dab
fverg <vergonaf@gmail.com>
2018-04-11 11:11:50 +0200

    First operation added

--- 01_vectors.py
+++ 01_vectors.py
@@ -1,1 +1,24 @@
-# Ex
+#
+
+import tensorflow as tf
+
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+res = tf.multiply(x1,x2)
+
+print(res)
+
+# The result will not be printed, that's because tensorflow implements lazy evalutation:
+# results doesn't get calculated until it isn't asked for itself.
+
+# So, we initialize a tensorflow session:
+
+s = tf.Session()
+
+# And then run it
+
+print(s.run(res))
+
+# At the end, we close it, releasing all the resources
+s.close()
